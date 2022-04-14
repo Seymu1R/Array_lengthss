eee96599aa34a4008ddd02fb0227ffa0c8ed4523
Seymu1R <102611811+Seymu1R@users.noreply.github.com>
2022-04-14 17:47:33 +0400

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            string[] arr = { "Seymur", "Elşən", "Fəxri", "Cavid", "Sabina", "Lamiya", "Ali" };
+            while (true)
+            {
+                if (arr[i].Length > 5)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+                i++;
+            }
+
+            
+        }
+    }
+}
